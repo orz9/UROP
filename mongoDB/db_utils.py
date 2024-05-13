734668b05221a41d3468902a19304770062b45e7
Elva <elvachen2000@gmail.com>
2024-05-13 16:04:53 +0800

    Updated mongoDB with different collections for more features

--- mongoDB/db_utils.py
+++ mongoDB/db_utils.py
@@ -1,16 +1,15 @@
 from .mongo_connection import get_mongo_client
 
 client = get_mongo_client()
-db = client['urop_user_info']  # Replace 'your_database_name' with the actual name of your database.
+db = client['urop-database']  # Replace 'your_database_name' with the actual name of your database.
 
-def check_user(username):
+def check_user(username, userAuthority):
     """Check if a user exists in the database."""
-    users_collection = db.users
-    user = users_collection.find_one({"username": username})
-    return user
+    users_collection = db['user-info'] if userAuthority == "student" else db['admin-info']
+    return users_collection.find_one({"username": username})
 
-def add_user(username, password):
+def add_user(username, password, userAuthority):
     """Add a new user to the database."""
-    users_collection = db.users
+    users_collection = db['user-info'] if userAuthority == "student" else db['admin-info']
     result = users_collection.insert_one({"username": username, "password": password})
     return result.inserted_id
